fe2859abbf115e76b1258cc2fa72199f90f0e029
ingjacome <ingjacome@gmail.com>
2026-01-22 15:35:00 -0500

    Update thermowell_sketch.py

--- modules/thermowell_sketch.py
+++ modules/thermowell_sketch.py
@@ -75,9 +75,9 @@ def plot_thermowell_datasheet(pipe_id_mm, H, U, L,pipe_od, connection_value):
     ax.text(-20, (pipe_radius + tw_end)/2, f"U = {U:.1f} mm", rotation=90, va="center", fontsize=8)
 
     # L
-    ax.annotate("", xy=(50, tw_start), xytext=(50, tw_end),
+    ax.annotate("", xy=(pipe_radius + 50, tw_start), xytext=(pipe_radius + 50, tw_end),
                 arrowprops=dict(arrowstyle="<->"))
-    ax.text(55, (tw_start + tw_end)/2, f"L = {L:.1f} mm", rotation=90, va="center", fontsize=8)
+    ax.text(pipe_radius + 60, (tw_start + tw_end)/2, f"L = {L:.1f} mm", rotation=90, va="center", fontsize=8)
 
     # =========================
     # VIEW
